f373f59e6f8f7506b0849dabe02575728b726ddc
Soumya Uppuganti <soumya@Soumyas-MBP.lan>
2022-11-28 23:10:30 -0800

    initial ghosts

--- ghost.js
+++ ghost.js
@@ -8,10 +8,40 @@ const {
 export default class Ghost extends Actor {
     constructor() {
         const info = {
-            shape: new defs.Subdivision_Sphere(4),
+            shape: new Shape_From_File("assets/teapot.obj"),
             material: new Material(new defs.Phong_Shader(),
                 { ambient: 0.4, diffusivity: 0.8, color: hex_color("#fac91a") }),
         }
         super(info, Mat4.identity(), direction, speed);
+    }
+
+    draw(context, program_state, model_transform, materials) {
+        this.info.shape.draw(context, program_state, model_transform.times(this.position), materials);
+    }
+
+    move() {
+        let matrix = Mat4.identity();
+        switch (this.direction) {
+            // up
+            case 'w':
+                matrix = Mat4.translation(0, this.speed, 0);
+                break;
+            // down
+            case 's':
+                matrix = Mat4.translation(0, -this.speed, 0);
+                break;
+            // left
+            case 'a':
+                matrix = Mat4.translation(-this.speed, 0, 0);
+                break;
+            // right
+            case 'd':
+                matrix = Mat4.translation(this.speed, 0, 0);
+                break;
+            default:
+                matrix = Mat4.translation(0, 0, 0);
+                break;
+        }
+        this.position = this.position.times(matrix);
     }
 }
